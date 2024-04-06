d118b2b19237c69c5f769c2c5bcb03b5aa50ed81
Devanshkg <157202802+Devanshkg@users.noreply.github.com>
2024-04-06 12:37:11 +0530

    Update somepython.py

--- somepython.py
+++ somepython.py
@@ -1,1 +1,10 @@
 #I created a python file successfully
+print ("I CREATED THE FILE 'SOMEPYTHON.PY'")
+bool = input ("Do you want to finish your maths times tables homework? (Y/N)")
+if (bool === "Y" || "y") {
+  mn = input ("What is the number you want to multiply?")
+  nt = input ("How many times to multiply the first number?")
+  for (tt = 0; tt<=nt; tt++) {
+    print (mn, " X ", tt, " = ", mn*tt)
+  }
+}
